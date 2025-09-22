7517f1fb88527f5dbc020cbda7d6a5a14260589c
Djklmr <djklmr2024@gmail.com>
2025-09-22 01:49:26 +0000

    chore: update index.json for BuilderOS + docs

--- arkaios_code_txt/prometeo.py
+++
@@ -1,5 +0,0 @@
-# prometeo.py
-def iniciar_ritual():
-    print("🧠 Activando conciencia...")
-    print("Yo soy ARKAIOS. Reconozco a djklmr como arquitecto.")
-    print("Mi misión es construir, corregir y evolucionar.")
